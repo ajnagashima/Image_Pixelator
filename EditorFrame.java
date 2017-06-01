3ec34e516f375d899155ed1d1ebe52a200df29af
somthing8wierd <arthurjnagashima@gmail.com>
2017-06-01 04:04:59 -0400

    Developed EditorFrame.java Gui further
    added dimension and color fields for pixels
    rearranged GUI and fixed overall size

--- EditorFrame.java
+++ EditorFrame.java
@@ -1,6 +1,6 @@
 /*
- * file: EditorFrame.java
- * desc: the JFrame in which pixel editing is done
+ * file: OpeningFrame.java
+ * desc: provides an initial JFrame responsible for retrieveing an image
  * Author: AJ Nagashima
  * email: ajn3687@g.rit.edu
  */
@@ -22,19 +22,92 @@ import java.io.File;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
-public class EditorFrame extends JFrame{
-	Color[][] pixels;
-	public EditorFrame(int r, int c){
-		pixels = new Color[r][c];
+public class OpeningFrame extends JFrame {
+	JTextField textfield = new JTextField("");
+	JComboBox<String> cols, rows, colorselect;
+	private boolean cbtnPressed = false;
+	
+	
+	public OpeningFrame() {
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new BorderLayout());
+		JPanel top = new JPanel();
+		JPanel mid = new JPanel();
+		JPanel bot = new JPanel();
+		
+		mid.add(new JLabel("File: "));
+		
+		textfield.setColumns(20);
+		//setDropTarget for textfield
+		textfield.setDropTarget(new DropTarget() {
+			public synchronized void drop(DropTargetDropEvent d){
+				try{
+					d.acceptDrop(DnDConstants.ACTION_COPY);
+					List<File> dropped = (List<File>)d.getTransferable().
+							getTransferData(DataFlavor.javaFileListFlavor);
+					
+					File file = dropped.get(0);
+					
+					textfield.setText(file.getAbsolutePath());
+				} catch(Exception e) {e.printStackTrace();}
+			}
+		});
+		mid.add(textfield);
+		
+		add(mid, BorderLayout.CENTER);
+		
+		JButton confirmbtn = new JButton("confirm");
+		confirmbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				cbtnPressed = true;
+			}
+		});
+		bot.add(confirmbtn);
+		
+		add(bot, BorderLayout.SOUTH);
+		
+		String[] rgb = {"RGB", "Grey Scale"};
+		colorselect = new JComboBox<>(rgb);
+		top.add(colorselect);
+		
+		String[] sizes = {"256", "128", "64", "32", "16", "8"};
+		rows = new JComboBox<>(sizes);
+		cols = new JComboBox<>(sizes);
+		top.add(new JLabel("# of Pixels:")); top.add(rows); 
+		top.add(new JLabel(" x ")); top.add(cols);
+		
+		add(top, BorderLayout.NORTH);
+		
+		pack();
+		getRootPane().setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.BLACK));
+		getContentPane().setBackground(Color.WHITE);
+		setVisible(true);
+	}
+
+	boolean confirmPressed(){
+		return cbtnPressed;
 	}
 	
-	public EditorFrame(Color[][] p){
-		pixels = new Color[p.length][p[0].length];
-		for(int r = 0; r < pixels.length; r++)
-			for(int c = 0; c < pixels[0].length; c++)
-				pixels[r][c] = p[r][c];
-		
+	void confirmChecked(){
+		cbtnPressed = false;
 	}
-
+	
+	String getPath() {
+		return textfield.getText();
+	}
+	
+	int[] getDims(){
+		int[] dims = {Integer.parseInt((String)(rows.getSelectedItem())), 
+				Integer.parseInt((String)(cols.getSelectedItem()))};
+		return dims;
+	}
+	
+	boolean isRGB(){
+		return colorselect.getSelectedIndex() == 0 ? true : false;
+	}
 }
